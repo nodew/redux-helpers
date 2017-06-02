68ab91942486b65cab58db71bf9c738b9ec10157
Joe Wang <wangqiao11@hotmail.com>
2017-06-02 11:15:39 +0800

    add build files and change entrypoint

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import {
+  createAsyncType,
+  createAsyncTypes,
+  createTypes
+} from './createTypes';
+
+import { 
+  createAction,
+  createActions,
+  createAsyncAction
+} from './createAction';
+
+import {
+  createReducer,
+  createAsyncReducer,
+} from './createReducer';
+
+export {
+  createAsyncType,
+  createAsyncTypes,
+  createTypes,
+
+  createAction,
+  createActions,
+  createAsyncAction,
+
+  createReducer,
+  createAsyncReducer
+}
